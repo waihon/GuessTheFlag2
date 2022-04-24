e833cce1c6b57f18d33ba47f9c9945fc2941486b
Waihon Yew <yewwaihon@gmail.com>
2022-04-24 17:47:34 +0800

    Initial Commit

---
+++ GuessTheFlag2/GuessTheFlag2App.swift
@@ -0,0 +1,17 @@
+//
+//  GuessTheFlag2App.swift
+//  GuessTheFlag2
+//
+//  Created by Waihon Yew on 24/04/2022.
+//
+
+import SwiftUI
+
+@main
+struct GuessTheFlag2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
