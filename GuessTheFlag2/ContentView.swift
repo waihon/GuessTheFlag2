35aeb40d4d9b49fcd6417dde3700e19a9b2ec6c2
Waihon Yew <yewwaihon@gmail.com>
2022-04-24 22:30:44 +0800

    Fill the whole area behind the text with red

--- GuessTheFlag2/ContentView.swift
+++ GuessTheFlag2/ContentView.swift
@@ -10,8 +10,8 @@ import SwiftUI
 struct ContentView: View {
     var body: some View {
         ZStack {
+            Color.red
             Text("Your content")
-                .background(.red)
         }
     }
 }
