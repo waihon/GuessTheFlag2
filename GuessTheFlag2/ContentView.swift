acd2031535a07399ab00c510da7ce109de2b8236
Waihon Yew <yewwaihon@gmail.com>
2022-04-27 21:43:58 +0800

    Create a button which shows an alert with a single button

--- GuessTheFlag2/ContentView.swift
+++ GuessTheFlag2/ContentView.swift
@@ -8,6 +8,19 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var showingAlert = false
+
+    var body: some View {
+        Button("Show Alert") {
+            showingAlert = true
+        }
+        .alert("Important message", isPresented: $showingAlert) {
+            Button("OK") { }
+        }
+    }
+}
+
+struct ButtonsAndImagesContentView: View {
     var body: some View {
         VStack {
             Button("Delete selection", role: .destructive, action: executeDelete)
