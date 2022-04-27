6231bf36ab9bded25eef1dce3c4d2580ad0db4b1
Waihon Yew <yewwaihon@gmail.com>
2022-04-28 00:20:35 +0800

    Declare instance variables

--- GuessTheFlag2/ContentView.swift
+++ GuessTheFlag2/ContentView.swift
@@ -8,6 +8,9 @@
 import SwiftUI
 
 struct ContentView: View {
+    var countries = ["Estonia", "France", "Germany", "Ireland", "Italy", "Nigeria", "Poland", "Russia", "Spain", "UK", "US"]
+    var correctAnswer = Int.random(in: 0...2)
+
     var body: some View {
         Text("Hello, world!")
             .padding()
